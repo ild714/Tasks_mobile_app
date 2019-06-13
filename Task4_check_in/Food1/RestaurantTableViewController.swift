fa42d0b293186abcdccd62880b294fe234a107b8
ildar777 <42585624+ildar777@users.noreply.github.com>
2019-06-13 08:48:55 +0300

    check_in_function

---
+++ Task4_check_in/Food1/RestaurantTableViewController.swift
@@ -0,0 +1,185 @@
+//
+//  RestaurantTableViewController.swift
+//  Food1
+//
+//  Created by Ильдар Нигметзянов on 10/06/2019.
+//  Copyright © 2019 Ильдар Нигметзянов. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    var restaurantIsVisited = Array(repeating:false,count:21)
+    
+    var restaurantNames = ["Cafe Deadend","Homei","Teakha","Cafe Loisl","Petite Oyster","For Kee Restaurant","Po's Atelier","Bourke Street Bakery","Haigh's Chocolate","Palomino Espresso","Upstate","Traif","Graham Avenue Meats","Waffle & Wolf","Five Leaves","Cafe Lore","Confessional","Barrafina","Donostia","Royal Oak","CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend","homei","teakha","cafeloisl","petiteoyster","forkeerestaurant","posatelier","bourkestreetbakery","haighschocolate", "palominoespresso","upstate","traif","grahamavenuemeats","wafflewolf","fiveleaves","cafelore","confessional","barrafina","donostia","royaloak","caskpubkitchen"]
+    
+    var restaurantLocation = ["Hong Kong","Hong Kong","Hong Kong","Hong Kong","Hong Kong","Hong Kong","Sydney","Sydney","Sydney","Sydney","New York","New York","New York","New York","New York","New York","New York","London","London","London","London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop","Cafe","Tea House","Austrian/ Causual Drink","French","Bakery","Bakery","Chocolate","Cafe","American/Seafood","American","American","Breakfast & Brunch","Coffee & Tea","Coffe & Tea","Latian American","Spanish","Spanish","Spanish","British","Thai"]
+    
+    //working with cell
+    override func tableView(_ tableView:UITableView,cellForRowAt indexPath:IndexPath) -> UITableViewCell{
+        
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier:cellIdentifier,for:indexPath) as! RestaurantTableViewCell
+        
+        cell.nameLabel.text=restaurantNames[indexPath.row]
+    cell.thumbnailImageView.image=UIImage(named:restaurantImages[indexPath.row])
+        cell.locationLabel.text=restaurantLocation[indexPath.row]
+        cell.typelabel.text=restaurantTypes[indexPath.row]
+        
+        if restaurantIsVisited[indexPath.row]{
+            cell.accessoryType = .checkmark
+        }
+        else{
+            cell.accessoryType = .none
+        }
+        cell.heartImageView.isHidden=restaurantIsVisited[indexPath.row] ? false : true
+        
+        return cell
+    }
+    
+    //section number
+    override func numberOfSections (in tableView:UITableView) -> Int {
+        return 1
+    }
+
+    //amount of row
+    override func tableView(_ tableView: UITableView,numberOfRowsInSection section:Int) ->Int {
+        return restaurantNames.count
+    }
+    //cancel action
+    override func tableView(_ tableView:UITableView,didSelectRowAt indexPath:IndexPath){
+        let optionMenu = UIAlertController(title:nil,message:"What do you want to do?",preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title:"Cancel",style: .cancel,handler: nil)
+        
+        optionMenu.addAction(cancelAction)
+        
+        //alert (number)
+        let callActionHandler = {(action:UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage,animated:true,completion: nil)
+        }
+        let callAction = UIAlertAction(title: "Call"+"123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        //let checkActionTitle = (restaurantIsVisited[indexPath.row]) ? "Undo Check in" : "Check in"
+        //check in
+        let checkInAction = UIAlertAction(title:"Check in",style: .default,handler: {
+            (action:UIAlertAction!) -> Void in
+            let cell = tableView.cellForRow(at:indexPath) as! RestaurantTableViewCell
+            
+            self.restaurantIsVisited[indexPath.row] = (self.restaurantIsVisited[indexPath.row]) ? false : true
+            
+            cell.heartImageView.isHidden=self.restaurantIsVisited[indexPath.row] ? false : true
+            
+            //cell.accessoryType=(self.restaurantIsVisited[indexPath.row] ? .none : .checkmark
+        
+        })
+        optionMenu.addAction(checkInAction)
+        
+        /*
+        let offcheckInAction = UIAlertAction(title:"Off Chech in",style:.default,handler:{
+            (action:UIAlertAction!) -> Void in
+            let cell = tableView.cellForRow(at:indexPath)
+            cell?.accessoryType  = .none
+            self.restaurantIsVisited[indexPath.row] = false
+        })
+        optionMenu.addAction(offcheckInAction)
+        */
+        if let popoverController = optionMenu.popoverPresentationController{
+            if let cell = tableView.cellForRow(at:indexPath){
+                popoverController.sourceView=cell
+                popoverController.sourceRect=cell.bounds
+            }
+        }
+        
+        present(optionMenu,animated: true,completion:nil)
+        tableView.deselectRow(at:indexPath,animated:false)
+    }
+    
+    // MARK: - Table view data source
+
+    /*
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    */
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
